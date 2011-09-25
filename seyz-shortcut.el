5c85c54843f09484dd1420c9b5474c93bb758579
Sandro Munda <sandro.munda@guardis.com>
2011-09-25 15:30:06 +0200

    Added some shortcuts :   - comment-region   - uncomment-region   - bury-buffer   - kill-this-buffer   - join-line   - copy-line (TODO: define the function !)   - toggle-fullscreen

---
+++ seyz-shortcut.el
@@ -0,0 +1,7 @@
+(global-set-key (kbd "C-c C-c") 'comment-region)
+(global-set-key (kbd "C-c M-c") 'uncomment-region)
+(global-set-key (kbd "C-c o") 'bury-buffer)
+(global-set-key (kbd "C-c k") 'kill-this-buffer)
+(global-set-key (kbd "C-c j") 'join-line)
+(global-set-key (kbd "C-c C-k") 'copy-line)
+(global-set-key [f11] 'toggle-fullscreen)
