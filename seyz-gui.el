cc9ba6fd383ad4db350b6fbd8fdcd516de34cbfd
Sandro Munda <sandro.munda@guardis.com>
2011-09-25 12:39:16 +0200

    Added the seyz-gui.el file Added the auto fullscreen custom "mode" Got rid of bars

---
+++ seyz-gui.el
@@ -0,0 +1,18 @@
+;; ------ Fullscreen ------
+(defun toggle-fullscreen (&optional f)
+  (interactive)
+  (let ((current-value (frame-parameter nil 'fullscreen)))
+    (set-frame-parameter nil 'fullscreen
+                         (if (equal 'fullboth current-value)
+                             (if (boundp 'old-fullscreen) old-fullscreen nil)
+                           (progn (setq old-fullscreen current-value)
+                                  'fullboth)))))
+
+;; turn on the fullscreen
+(run-with-idle-timer 0.1 nil 'toggle-fullscreen)
+
+;; ------ Get rid of bars ------
+(progn
+  (menu-bar-mode)
+  (tool-bar-mode)
+  (scroll-bar-mode))
