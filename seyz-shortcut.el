d46abddb235ce8b625935489ca4057a0f68395f8
Sandro Munda <sandro.munda@guardis.com>
2011-09-25 16:10:25 +0200

    Bound the <f1> key to the 'ansi-term function

--- seyz-shortcut.el
+++ seyz-shortcut.el
@@ -4,4 +4,6 @@
 (global-set-key (kbd "C-c k") 'kill-this-buffer)
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-c C-k") 'copy-line)
+
+(global-set-key [f1] 'ansi-term)
 (global-set-key [f11] 'toggle-fullscreen)
