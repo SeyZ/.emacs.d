44d5b7ac2aaec133c07f3af81d96fe959c160b3e
Sandro Munda <sandro.munda@guardis.com>
2011-10-16 15:33:04 +0200

    Added the js2-mode for Javascript language

--- seyz-lang.el
+++ seyz-lang.el
@@ -3,3 +3,7 @@
 ;; ------ Java ------
 (add-hook 'java-mode-hook
 	  (lambda () (load-library "seyz-java")))
+
+;; ------ Javascript ------
+(autoload 'js2-mode "js2" nil t)
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
