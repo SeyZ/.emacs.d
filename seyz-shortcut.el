6f94c000268934e75c2456a5d3db770b29e05d62
Sandro Munda <sandro.munda@guardis.com>
2011-10-22 14:40:55 +0200

    Bound the C-c M-s to the occur function

--- seyz-shortcut.el
+++ seyz-shortcut.el
@@ -4,6 +4,7 @@
 (global-set-key (kbd "C-c k") 'kill-this-buffer)
 (global-set-key (kbd "C-c j") 'join-line)
 (global-set-key (kbd "C-c C-k") 'copy-line)
+(global-set-key (kbd "C-c M-s") 'occur)
 
 (global-set-key [f1] 'ansi-term)
 (global-set-key [f11] 'toggle-fullscreen)
