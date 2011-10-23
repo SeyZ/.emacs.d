c9181dac2a6643007c68e163e7372c628f7f0134
Sandro Munda <sandro.munda@guardis.com>
2011-10-23 17:26:09 +0200

    Bound the RET key to the new-line-and-indent function Replaced join-line by delete-indentation (default in emacs)

--- seyz-shortcut.el
+++ seyz-shortcut.el
@@ -2,9 +2,10 @@
 (global-set-key (kbd "C-c M-c") 'uncomment-region)
 (global-set-key (kbd "C-c o") 'bury-buffer)
 (global-set-key (kbd "C-c k") 'kill-this-buffer)
-(global-set-key (kbd "C-c j") 'join-line)
+(global-set-key (kbd "C-c j") 'delete-indentation)
 (global-set-key (kbd "C-c C-k") 'copy-line)
 (global-set-key (kbd "C-c M-s") 'occur)
+(global-set-key (kbd "RET") 'newline-and-indent)
 
 (global-set-key [f1] 'ansi-term)
 (global-set-key [f11] 'toggle-fullscreen)
