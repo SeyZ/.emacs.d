51a3f361d03b745c4ca050e591e8e69e7a48b2b4
Sandro Munda <sandro.munda@guardis.com>
2011-10-23 17:26:33 +0200

    Reconfigured the ido mode

--- seyz-ido.el
+++ seyz-ido.el
@@ -1,3 +1,11 @@
-(ido-mode t)
-(setq ido-enable-flex-matching t)
-(setq tramp-mode nil)
+(ido-mode t)                            ; Enable the ido mode globally
+(setq ido-everywhere t)                 ; Enable ido everywhere
+(setq ido-enable-flex-matching t)       ; Enable the flex matching
+(setq tramp-mode nil)                   ; Disable the tramp mode
+
+(setq ido-auto-merge-delay-time 0)      ; No delay to the complete feature
+
+(define-key ido-file-dir-completion-map (kbd "C-c C-s")
+  (lambda()
+    (interactive)
+    (ido-initiate-auto-merge (current-buffer))))
