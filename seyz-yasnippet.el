7308ab2263889874b142ee8371c07ec529e8c56e
Sandro Munda <sandro.munda@guardis.com>
2011-10-23 17:25:37 +0200

    Disabled temporary yas-jit

--- seyz-yasnippet.el
+++ seyz-yasnippet.el
@@ -1,7 +1,13 @@
-(setq my-snippets-dir (concat my-vendor-emacs-dir "yasnippet/snippets/text-mode/"))
+;; (setq my-snippets-dir (concat my-vendor-emacs-dir "yasnippet/snippets/text-mode/"))
+(setq my-snippets-dir (concat my-vendor-emacs-dir "yasnippet/snippets/"))
 
-(require 'yas-jit)
-(setq yas/root-directory my-snippets-dir)
-(yas/jit-load)
+(require 'yasnippet)
+(yas/initialize)
+(yas/load-directory my-snippets-dir)
 
-(yas/global-mode)
+
+;; (require 'yas-jit)
+;; (setq yas/root-directory my-snippets-dir)
+;; (yas/jit-load)
+
+(yas/global-mode t)
